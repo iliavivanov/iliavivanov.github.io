a964fab5a8921c912017ad0b84b5765564aef1c9
ilivanov <ilivanov@mera.ru>
2015-07-22 16:15:55 +0300

    Debig webqr

--- resources/js/webqr/webqr.js
+++ resources/js/webqr/webqr.js
@@ -3,68 +3,62 @@
 
 var gCtx = null;
 var gCanvas = null;
-var c=0;
-var stype=0;
-var gUM=false;
-var webkit=false;
-var moz=false;
-var v=null;
+var c = 0;
+var stype = 0;
+var gUM = false;
+var webkit = false;
+var moz = false;
+var v = null;
 
-var imghtml='<div id="qrfile"><canvas id="out-canvas" width="320" height="240"></canvas>'+
-    '<div id="imghelp">drag and drop a QRCode here'+
-	'<br>or select a file'+
-	'<input type="file" onchange="handleFiles(this.files)"/>'+
-	'</div>'+
-'</div>';
+var imghtml = '<div id="qrfile"><canvas id="out-canvas" width="320" height="240"></canvas>' +
+    '<div id="imghelp">drag and drop a QRCode here' +
+    '<br>or select a file' +
+    '<input type="file" onchange="handleFiles(this.files)"/>' +
+    '</div>' +
+    '</div>';
 
 var vidhtml = '<video id="v" autoplay></video>';
 
 function dragenter(e) {
-  e.stopPropagation();
-  e.preventDefault();
+    e.stopPropagation();
+    e.preventDefault();
 }
 
 function dragover(e) {
-  e.stopPropagation();
-  e.preventDefault();
+    e.stopPropagation();
+    e.preventDefault();
 }
 function drop(e) {
-  e.stopPropagation();
-  e.preventDefault();
+    e.stopPropagation();
+    e.preventDefault();
 
-  var dt = e.dataTransfer;
-  var files = dt.files;
-  if(files.length>0)
-  {
-	handleFiles(files);
-  }
-  else
-  if(dt.getData('URL'))
-  {
-	qrcode.decode(dt.getData('URL'));
-  }
-}
-
-function handleFiles(f)
-{
-	var o=[];
-	
-	for(var i =0;i<f.length;i++)
-	{
-        var reader = new FileReader();
-        reader.onload = (function(theFile) {
-        return function(e) {
-            gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
-
-			qrcode.decode(e.target.result);
-        };
-        })(f[i]);
-        reader.readAsDataURL(f[i]);	
+    var dt = e.dataTransfer;
+    var files = dt.files;
+    if (files.length > 0) {
+        handleFiles(files);
+    }
+    else if (dt.getData('URL')) {
+        qrcode.decode(dt.getData('URL'));
     }
 }
 
-function initCanvas(w,h)
-{
+function handleFiles(f) {
+    var o = [];
+
+    for (var i = 0; i < f.length; i++) {
+        var reader = new FileReader();
+        reader.onload = (function (theFile) {
+            return function (e) {
+                gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
+
+                qrcode.decode(e.target.result);
+            };
+        })(f[i]);
+        reader.readAsDataURL(f[i]);
+    }
+}
+
+function initCanvas(w, h) {
     gCanvas = document.getElementById("qr-canvas");
     gCanvas.style.width = w + "px";
     gCanvas.style.height = h + "px";
@@ -76,24 +70,25 @@ function initCanvas(w,h)
 
 
 function captureToCanvas() {
-    if(stype!=1)
+    if (stype != 1)
         return;
-    if(gUM)
-    {
-        try{
-            gCtx.drawImage(v,0,0);
-            try{
+    if (gUM) {
+        try {
+            gCtx.drawImage(v, 0, 0);
+            try {
                 qrcode.decode();
             }
-            catch(e){       
+            catch (e) {
                 console.log(e);
                 setTimeout(captureToCanvas, 500);
-            };
+            }
+            ;
         }
-        catch(e){       
-                console.log(e);
-                setTimeout(captureToCanvas, 500);
-        };
+        catch (e) {
+            console.log(e);
+            setTimeout(captureToCanvas, 500);
+        }
+        ;
     }
 }
 
@@ -101,62 +96,56 @@ function htmlEntities(str) {
     return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 }
 
-function read(a)
-{
-    var html="<br>";
-    if(a.indexOf("http://") === 0 || a.indexOf("https://") === 0)
-        html+="<a target='_blank' href='"+a+"'>"+a+"</a><br>";
-    html+="<b>"+htmlEntities(a)+"</b><br><br>";
-    document.getElementById("result").innerHTML=html;
-}	
+function read(a) {
+    var html = "<br>";
+    if (a.indexOf("http://") === 0 || a.indexOf("https://") === 0)
+        html += "<a target='_blank' href='" + a + "'>" + a + "</a><br>";
+    html += "<b>" + htmlEntities(a) + "</b><br><br>";
+    document.getElementById("result").innerHTML = html;
+}
 
-function isCanvasSupported(){
-  var elem = document.createElement('canvas');
-  return !!(elem.getContext && elem.getContext('2d'));
+function isCanvasSupported() {
+    var elem = document.createElement('canvas');
+    return !!(elem.getContext && elem.getContext('2d'));
 }
 function success(stream) {
-    if(webkit)
+    if (webkit)
         v.src = window.webkitURL.createObjectURL(stream);
-    else
-    if(moz)
-    {
+    else if (moz) {
         v.mozSrcObject = stream;
         v.play();
     }
     else
         v.src = stream;
-    gUM=true;
+    gUM = true;
     setTimeout(captureToCanvas, 500);
 }
-		
+
 function error(error) {
-    gUM=false;
+    gUM = false;
     return;
 }
 
-function load()
-{
-	if(isCanvasSupported() && window.File && window.FileReader)
-	{
-		initCanvas(800, 600);
-		qrcode.callback = read;
-		document.getElementById("mainbody").style.display="inline";
+function load() {
+    if (isCanvasSupported() && window.File && window.FileReader) {
+        initCanvas(800, 600);
+        qrcode.callback = read;
+        document.getElementById("mainbody").style.display = "inline";
         setwebcam();
-	}
-	else
-	{
-		document.getElementById("mainbody").style.display="inline";
-		document.getElementById("mainbody").innerHTML='<p id="mp1">QR code scanner for HTML5 capable browsers</p><br>'+
-        '<br><p id="mp2">sorry your browser is not supported</p><br><br>'+
+    }
+    else {
+        document.getElementById("mainbody").style.display = "inline";
+        document.getElementById("mainbody").innerHTML = '<p id="mp1">QR code scanner for HTML5 capable browsers</p><br>' +
+        '<br><p id="mp2">sorry your browser is not supported</p><br><br>' +
         '<p id="mp1">try <a href="http://www.mozilla.com/firefox"><img src="firefox.png"/></a> or <a href="http://chrome.google.com"><img src="chrome_logo.gif"/></a> or <a href="http://www.opera.com"><img src="Opera-logo.png"/></a></p>';
-	}
+    }
 }
 
 var constraints = {
     audio: false,
     video: {
         optional: [{
-            sourceId: new DOMString("291f33b383d8484951c84f38d5f743709aeff8bf3b5f1b1e9aec1273a3376ed0")
+            sourceId: "291f33b383d8484951c84f38d5f743709aeff8bf3b5f1b1e9aec1273a3376ed0"
         }]
         //mandatory: {
         //    maxWidth: 640,
@@ -203,44 +192,38 @@ function getUserMedia() {
     }
 }
 
-function setwebcam()
-{
-	document.getElementById("result").innerHTML="- scanning -";
-    if(stype==1)
-    {
-        setTimeout(captureToCanvas, 500);    
+function setwebcam() {
+    document.getElementById("result").innerHTML = "- scanning -";
+    if (stype == 1) {
+        setTimeout(captureToCanvas, 500);
         return;
     }
-    var n=navigator;
+    var n = navigator;
     document.getElementById("outdiv").innerHTML = vidhtml;
-    v=document.getElementById("v");
+    v = document.getElementById("v");
 
     //alert("1");
 
 
     //getUserMedia();
-/*
-    if (typeof MediaStreamTrack === 'undefined') {
-        alert('This browser does not support MediaStreamTrack!\n\nTry Chrome Canary!');
-        getUserMedia();
-        alert("2");
-    } else {
-        MediaStreamTrack.getSources(onSourcesAcquired);
-        alert("3");
-    }
-*/
-    if(n.getUserMedia)
+    /*
+     if (typeof MediaStreamTrack === 'undefined') {
+     alert('This browser does not support MediaStreamTrack!\n\nTry Chrome Canary!');
+     getUserMedia();
+     alert("2");
+     } else {
+     MediaStreamTrack.getSources(onSourcesAcquired);
+     alert("3");
+     }
+     */
+    if (n.getUserMedia)
         n.getUserMedia(constraints, success, error);
-    else
-    if(n.webkitGetUserMedia)
-    {
-        webkit=true;
+    else if (n.webkitGetUserMedia) {
+        webkit = true;
         n.webkitGetUserMedia(constraints, success, error);
     }
-    else
-    if(n.mozGetUserMedia)
-    {
-        moz=true;
+    else if (n.mozGetUserMedia) {
+        moz = true;
         n.mozGetUserMedia(constraints, success, error);
     }
 
@@ -248,25 +231,24 @@ function setwebcam()
 
     //document.getElementById("qrimg").src="qrimg2.png";
     //document.getElementById("webcamimg").src="webcam.png";
-    document.getElementById("qrimg").style.opacity=0.2;
-    document.getElementById("webcamimg").style.opacity=1.0;
+    document.getElementById("qrimg").style.opacity = 0.2;
+    document.getElementById("webcamimg").style.opacity = 1.0;
 
-    stype=1;
+    stype = 1;
     setTimeout(captureToCanvas, 1500);
 }
-function setimg()
-{
-	document.getElementById("result").innerHTML="";
-    if(stype==2)
+function setimg() {
+    document.getElementById("result").innerHTML = "";
+    if (stype == 2)
         return;
     document.getElementById("outdiv").innerHTML = imghtml;
     //document.getElementById("qrimg").src="qrimg.png";
     //document.getElementById("webcamimg").src="webcam2.png";
-    document.getElementById("qrimg").style.opacity=1.0;
-    document.getElementById("webcamimg").style.opacity=0.2;
+    document.getElementById("qrimg").style.opacity = 1.0;
+    document.getElementById("webcamimg").style.opacity = 0.2;
     var qrfile = document.getElementById("qrfile");
-    qrfile.addEventListener("dragenter", dragenter, false);  
-    qrfile.addEventListener("dragover", dragover, false);  
+    qrfile.addEventListener("dragenter", dragenter, false);
+    qrfile.addEventListener("dragover", dragover, false);
     qrfile.addEventListener("drop", drop, false);
-    stype=2;
+    stype = 2;
 }
